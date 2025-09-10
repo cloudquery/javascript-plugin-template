c156c63b4e7d93923bd5d41709b54e7b9eee6936
pilvikala <michal@cloudquery.io>
2025-09-10 16:53:35 +0300

    update dependencies, make better example spec

--- src/spec.ts
+++ src/spec.ts
@@ -5,7 +5,9 @@ const spec = {
   type: "object",
   properties: {
     concurrency: { type: "integer" },
+    exampleProperty: { type: "string" },
   },
+  required: ["example_property"],
 };
 
 const ajv = new Ajv.default();
@@ -13,6 +15,7 @@ const validate = ajv.compile(spec);
 
 export type Spec = {
   concurrency: number;
+  exampleProperty: string;
 };
 
 export const parseSpec = (spec: string): Spec => {
@@ -21,8 +24,7 @@ export const parseSpec = (spec: string): Spec => {
   if (!valid) {
     throw new Error(`Invalid spec: ${JSON.stringify(validate.errors)}`);
   }
-  const {
-    concurrency = 10_000,
-  } = camelcaseKeys(parsed);
-  return { concurrency };
+  const { concurrency = 10_000, exampleProperty = "value" } =
+    camelcaseKeys(parsed);
+  return { concurrency, exampleProperty };
 };
